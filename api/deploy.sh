ff5670691ccaa3fe1398491cbd3a114882894e58
Jordan Denison <jordan@denisonweb.com>
2023-11-19 00:18:43 +0000

    update deploy script and package-lock.json

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker exec -it supanotes-api-1 npm run compile && heroku container:push web -a supanotes && heroku container:release web -a supanotes
+docker exec -it supanotes-api-1 npm run bundle:client && heroku container:push web -a supanotes && heroku container:release web -a supanotes
